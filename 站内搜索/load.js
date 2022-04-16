4027a6f6c7af39aa96b28d7a140f926b53ce1050
wen <101689790+wen53231323@users.noreply.github.com>
2022-04-17 01:37:48 +0800

    Add files via upload

---
+++ 站内搜索/load.js
@@ -0,0 +1,6 @@
+// 加载search.js调用searchFunc()
+if ($('.local-search')) {
+  $.getScript('/custom/站内搜索/search.js', function() {
+    searchFunc("/search.xml", 'local-search-input', 'local-search-result');
+  });
+}
